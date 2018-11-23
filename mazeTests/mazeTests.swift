149dc6bc3ae455810771eec78b1cf02172af8a73
20161104616 <992846873@qq.com>
2018-11-23 17:25:03 +0800

    Initial Commit

---
+++ mazeTests/mazeTests.swift
@@ -0,0 +1,34 @@
+//
+//  mazeTests.swift
+//  mazeTests
+//
+//  Created by 20161104616 on 2018/11/23.
+//  Copyright © 2018 20161104616. All rights reserved.
+//
+
+import XCTest
+@testable import maze
+
+class mazeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
